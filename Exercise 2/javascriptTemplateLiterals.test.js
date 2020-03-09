ec293f4b53ec685bb3819704fd24bf8ee0a9edc8
SenaGust <gustavosena98@live.com>
2020-03-09 16:18:32 -0300

    Trying learn mock function

--- Exercise 2/javascriptTemplateLiterals.test.js
+++ Exercise 2/javascriptTemplateLiterals.test.js
@@ -9,18 +9,19 @@ describe('function HowOldAre', () => {
 
 describe('function Write', () => {
     it('should show the result mocking HowOldAre', () => {
-        console.log = jest.fn()
-        const HowOldAre = jest.fn();
+        
+        console.log = jest.fn();
+
+        const HowOldAre = jest.fn(person => `${person.name} is ${person.age} years old`);
         HowOldAre.mockReturnValueOnce("John is 30 years old")
         .mockReturnValueOnce("Marie is 22 years old")
         .mockReturnValueOnce("August is 30 years old");
-
-        Write();
-        expect(console.log).toHaveBeenCalledTimes(3)
-        expect(console.log).toHaveBeenNthCalledWith(1,"John is 30 years old")
-        expect(console.log).toHaveBeenNthCalledWith(2,"Marie is 22 years old")
-        expect(console.log).toHaveBeenNthCalledWith(3,"August is 30 years old")
         
+        Write(HowOldAre);
+        expect(HowOldAre).toHaveBeenCalledTimes(3);
+        expect(HowOldAre).toHaveBeenNthCalledWith(1,"John is 30 years old");
+        expect(HowOldAre).toHaveBeenNthCalledWith(2,"Marie is 22 years old");
+        expect(HowOldAre).toHaveBeenNthCalledWith(3,"August is 30 years old");
     })
 });
 
