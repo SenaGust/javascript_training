019e3886231930a1823ac4b9a514dc7c126f5f29
SenaGust <gustavosena98@live.com>
2020-03-13 14:01:36 -0300

    formas corretas implementação do throw

--- Exercise 4/multiplication.js
+++ Exercise 4/multiplication.js
@@ -1,12 +1,8 @@
 function multiply(num1, num2) {
-    try {
-        if (isNaN(num1) || isNaN(num2)) {
-            throw "Parameter is not a number!";
-          }
-        return num1 * num2;
-    } catch (error) {
-        return error.message;
+    if (isNaN(num1) || isNaN(num2)) {
+        throw "TypeError";
     }
+    return num1 * num2;
 };
 
-module.exports = {multiply};
+module.exports = { multiply };
--- Exercise 4/multiplication.test.js
+++ Exercise 4/multiplication.test.js
@@ -11,18 +11,23 @@ describe('function multiply', () => {
     });
     it('should return error when I pass with \'a\' and 5', () => {
         const parameter1 = 'a', parameter2 = 5
-        expectedResult = 'NaN';
-
-        const result = multiply(parameter1, parameter2);
+        const expectedResult = 'TypeError';
+        try {
+            multiply(parameter1, parameter2)
+            fail();
+        } catch (error) {
+            expect(error).toEqual(expectedResult);    
+        }
         
-        expect(result).toThrow(expectedResult);
     });
     it('should return error when I pass with 2 and b', () => {
         const parameter1 = 2, parameter2 = 'b'
-        expectedResult = 'NaN';
-
-        const result = multiply(parameter1, parameter2);
-        
-        expect(result).toThrow(expectedResult);
+        expectedResult = 'TypeError';
+        try {
+            multiply(parameter1, parameter2)
+            fail();
+        } catch (error) {
+            expect(error).toEqual(expectedResult);    
+        }
     });
 });
