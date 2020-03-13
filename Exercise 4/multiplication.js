2efd8a820d96e13be235be5979180081109fc7da
SenaGust <gustavosena98@live.com>
2020-03-13 10:45:58 -0300

    trying use tryCatch in test and code

--- Exercise 4/multiplication.js
+++ Exercise 4/multiplication.js
@@ -1,8 +1,11 @@
 function multiply(num1, num2) {
     try {
+        if (isNaN(num1) || isNaN(num2)) {
+            throw "Parameter is not a number!";
+          }
         return num1 * num2;
     } catch (error) {
-        throw(error);
+        return error.message;
     }
 };
 
--- Exercise 4/multiplication.test.js
+++ Exercise 4/multiplication.test.js
@@ -9,3 +9,20 @@ describe('function multiply', () => {
 
         expect(result).toBe(expectedResult);
     });
+    it('should return error when I pass with \'a\' and 5', () => {
+        const parameter1 = 'a', parameter2 = 5
+        expectedResult = 'NaN';
+
+        const result = multiply(parameter1, parameter2);
+        
+        expect(result).toThrow(expectedResult);
+    });
+    it('should return error when I pass with 2 and b', () => {
+        const parameter1 = 2, parameter2 = 'b'
+        expectedResult = 'NaN';
+
+        const result = multiply(parameter1, parameter2);
+        
+        expect(result).toThrow(expectedResult);
+    });
+});
