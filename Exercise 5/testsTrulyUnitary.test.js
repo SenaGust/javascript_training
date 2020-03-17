1b2adfb818229bd49f7641adcebeca2166310405
SenaGust <gustavosena98@live.com>
2020-03-17 15:24:39 -0300

    Correct spyOn

--- Exercise 5/testsTrulyUnitary.test.js
+++ Exercise 5/testsTrulyUnitary.test.js
@@ -1,5 +1,5 @@
 const {makeString} = require('./testsTrulyUnitary');
-const multiply = require('../Exercise 4/multiplication')
+const multiplication = require('../Exercise 4/multiplication')
 
 describe('function makeString', () => {
     it('should return the string "2 * 1 = 2" ', () => {
@@ -7,11 +7,13 @@ describe('function makeString', () => {
         parameter2 = '2',
         parameter3 = '1',
         expectedResult = '2 * 1 = 2';
+        const multiply = jest.spyOn(multiplication, 'multiply')
+        .mockImplementation((num1, num2) => num1 * num2);
 
-        const multiply = jest.fn((parameter2, parameter3) => (parameter2 * parameter3));
-        console.log(multiply(1,2));
-        
         const result = makeString(parameter1, parameter2, parameter3);
+
+        expect(multiply).toHaveBeenCalledWith(parameter2, parameter3);
+        expect(multiply).toHaveBeenCalledTimes(1);
         expect(result).toEqual(expectedResult);
     })
 })
