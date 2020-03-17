be2824599a010d77a42391c4f3b3af380b3d68cd
SenaGust <gustavosena98@live.com>
2020-03-17 15:34:06 -0300

    it's necessary for spyOn

--- Exercise 5/testsTrulyUnitary.js
+++ Exercise 5/testsTrulyUnitary.js
@@ -1,7 +1,7 @@
-const {multiply} = require('../Exercise 4/multiplication');
+const multiplication = require('../Exercise 4/multiplication');
 
 function makeString(string, number1, number2) {
-    return (string + multiply(number1, number2));
+    return (string +  multiplication.multiply(number1, number2));
 }
 
 module.exports = {makeString};
