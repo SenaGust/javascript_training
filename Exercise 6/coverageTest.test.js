5b4d1578462b0f2258a08488f823eb37cb1252d1
SenaGust <gustavosena98@live.com>
2020-03-18 10:01:30 -0300

    implementation of test

--- Exercise 6/coverageTest.test.js
+++ Exercise 6/coverageTest.test.js
@@ -19,5 +19,22 @@ describe('function makeString for exercise 6', () => {
         expect(Math.pow).toHaveBeenNthCalledWith(1, parameter2, 1/2);
         expect(Math.pow).toHaveBeenNthCalledWith(2, parameter3, 1/2);
         expect(result).toEqual(expectedResult);
-    })
+    });
+    it('should return the string "O resultado é: 2" (not using if) ', () => {
+        const parameter1 = 'O resultado é: ',
+        parameter2 = 2,
+        parameter3 = 1,
+        expectedResult = 'O resultado é: 2';
+
+        const multiply = jest.spyOn(multiplication, 'multiply')
+        .mockImplementation((num1, num2) => num1 * num2);
+        Math.pow = jest.fn();
+
+        const result = makeString(parameter1, parameter2, parameter3);
+
+        expect(Math.pow).toHaveBeenCalledTimes(0);
+        expect(multiply).toHaveBeenCalledTimes(1);
+        expect(multiply).toHaveBeenNthCalledWith(1, parameter2, parameter3);
+        expect(result).toEqual(expectedResult);
+    });
 })
