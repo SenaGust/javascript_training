8aceaa34950c8467f06e5dd55f9b9efa9f6046de
SenaGust <gustavosena98@live.com>
2020-03-18 12:20:47 -0300

    using try catch e create a function for math.pow

--- Exercise 5/testsTrulyUnitary.js
+++ Exercise 5/testsTrulyUnitary.js
@@ -1,7 +1,11 @@
 const multiplication = require('../Exercise 4/multiplication');
 
 function makeString(string, number1, number2) {
-    return (string +  multiplication.multiply(number1, number2));
+    try {
+        return (string +  multiplication.multiply(number1, number2));
+    } catch (error) {
+        return 'Please, try again because the second and/or third parameters must be numbers.';
+    }   
 }
 
 module.exports = {makeString};
--- Exercise 6/coverageTest.js
+++ Exercise 6/coverageTest.js
@@ -1,11 +1,22 @@
 const multiplication = require('../Exercise 4/multiplication');
 
 function makeString(string, number1, number2) {
-    if(number1 % 2 === 0 && number2 % 2 === 0) {
-        return (string + (Math.pow(number1, 1/2) + Math.pow(number2, 1/2)));
+    try {
+        if(number1 % 2 === 0 && number2 % 2 === 0) {
+            return (string + sumSquare(number1, number2));
+        }
+    
+        return (string +  multiplication.multiply(number1, number2));
+    } catch (error) {
+        return 'Please, try again because the second and/or third parameters must be numbers.';
     }
-
-    return (string +  multiplication.multiply(number1, number2));
 }
 
-module.exports = {makeString};
+function sumSquare(number1, number2) {
+    if (isNaN(number1) || isNaN(number1)) {
+        throw "TypeError";
+    }
+    return (Math.pow(number1, 1/2) + Math.pow(number2, 1/2));
+}
+
+module.exports = {makeString, sumSquare};
