2c297e44367dc32363dad19b28fff124b9e4faba
SenaGust <gustavosena98@live.com>
2020-03-19 11:42:08 -0300

    make test for catch a error

--- Exercise 5/testsTrulyUnitary.test.js
+++ Exercise 5/testsTrulyUnitary.test.js
@@ -3,19 +3,52 @@ import makeString from './testsTrulyUnitary';
 import multiply from '../Exercise 4/multiplication';
 jest.mock('../Exercise 4/multiplication');
 
+beforeEach(() => {
+    multiply.mockClear();
+
+    multiply.mockImplementation((num1, num2) => {
+        if (isNaN(num1) || isNaN(num2)) {
+            throw "TypeError";
+        }
+        return num1 * num2;
+    });
+});
+
 describe('function makeString', () => {
     it('should return the string "2 * 1 = 2" ', () => {
         const parameter1 = '2 * 1 = ',
-        parameter2 = '2',
-        parameter3 = '1',
+        parameter2 = 2,
+        parameter3 = 1,
         expectedResult = '2 * 1 = 2';
-        const multiply = jest.spyOn(multiplication, 'multiply')
-        .mockImplementation((num1, num2) => num1 * num2);
+
+        const result = makeString(parameter1, parameter2, parameter3);
+        
+        expect(multiply).toHaveBeenCalledTimes(1);
+        expect(multiply).toHaveBeenCalledWith(parameter2, parameter3);
+        expect(result).toEqual(expectedResult);
+    });
+    it('should return error', () => {
+        const parameter1 = '2 * 1 = ',
+        parameter2 = 'q',
+        parameter3 = 1,
+        expectedResult = 'Please, try again because the second and/or third parameters must be numbers.';
 
         const result = makeString(parameter1, parameter2, parameter3);
 
         expect(multiply).toHaveBeenCalledTimes(1);
         expect(multiply).toHaveBeenCalledWith(parameter2, parameter3);
         expect(result).toEqual(expectedResult);
-    })
-})
+    });
+    it('should return error', () => {
+        const parameter1 = '2 * 1 = ',
+        parameter2 = 1,
+        parameter3 = 'q',
+        expectedResult = 'Please, try again because the second and/or third parameters must be numbers.';
+
+        const result = makeString(parameter1, parameter2, parameter3);
+
+        expect(multiply).toHaveBeenCalledTimes(1);
+        expect(multiply).toHaveBeenCalledWith(parameter2, parameter3);
+        expect(result).toEqual(expectedResult);
+    });
+});
