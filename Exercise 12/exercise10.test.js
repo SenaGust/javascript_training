a007efee5453f671f85a7b08046fd6c9d752f28c
SenaGust <gustavosena98@live.com>
2020-04-01 14:58:10 -0300

    exercise 10 done

---
+++ Exercise 12/exercise10.test.js
@@ -0,0 +1,62 @@
+import sortedCharactersLodash from './exercise10';
+import arraySortedNames from '../Breaking Bad API/sortedNames';
+import mockCharacters from '../Breaking Bad API/jsonCharacters';
+import returnAllCharacters from '../Exercise 9/getAllCharacters';
+jest.mock('../Exercise 9/getAllCharacters');
+
+describe('function sortedCharacters', () => {
+    it('should return all sorted api\'s names', async () => {
+        returnAllCharacters.mockResolvedValue(mockCharacters);
+
+        const result = await sortedCharactersLodash();
+        
+        expect(result).toEqual(arraySortedNames);
+    });
+    it('should return all sorted letters', async () => {
+        const alphabet = [
+            { name: 'X'}, { name:'N'}, { name:'a'}, { name:'P'}, { name:'b'}, { name:'Y'}, { name:'F'}, { name:'c'},
+            { name:'J'}, { name:'i'}, { name:'d'}, { name:'j'}, { name:'A'}, { name:'k'}, { name:'l'}, { name:'R'},
+            { name:'M'}, { name:'B'}, { name:'m'}, { name:'G'}, { name:'K'}, { name:'n'}, { name:'E'}, { name:'o'},
+            { name:'p'}, { name:'q'}, { name:'Q'}, { name:'r'}, { name:'s'}, { name:'I'}, { name:'S'}, { name:'D'},
+            { name:'t'}, { name:'W'}, { name:'O'}, { name:'u'}, { name:'L'}, { name:'v'}, { name:'w'}, { name:'U'},
+            { name:'x'}, { name:'y'}, { name:'z'}, { name:'H'}, { name:'Z'}, {name: 'V'}, {name: 'T'}, {name: 'C'},
+            {name: 'e'}, {name: 'f'}, {name: 'g'}, {name: 'h'},  
+          ],
+          expectedResult=[
+              "a", "A", "b", "B", "c", "C", "d", "D", 
+          "e", "E", "f", "F", "g", "G", "h", "H", "i", "I", "j", 
+          "J", "k", "K", "l", "L", "m", "M", "n", "N", "o", "O", 
+          "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "u", 
+          "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z"
+        ];
+
+        returnAllCharacters.mockResolvedValue(shuffle(alphabet));
+
+        const result = await sortedCharactersLodash();
+
+        expect(result).toEqual(expectedResult);
+    });
+});
+
+
+
+
+
+
+function shuffle(array) {
+    var m = array.length, t, i;
+  
+    // While there remain elements to shuffle…
+    while (m) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+  
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+  
+    return array;
+  }
