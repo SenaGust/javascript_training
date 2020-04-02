be7172a167e649268940676569e56dd648a05562
SenaGust <gustavosena98@live.com>
2020-04-02 11:21:20 -0300

    using try catch

--- Exercise 10/sortedCharacters.js
+++ Exercise 10/sortedCharacters.js
@@ -1,15 +1,19 @@
 import returnAllCharacters from '../Exercise 9/getAllCharacters';
 
 async function sortedCharacters() {
-    const allInformation = (await returnAllCharacters());
+    try {
+        const allInformation = (await returnAllCharacters());
 
-    const onlyNames = allInformation.map(data => {
-        return data.name;
-    });
+        const onlyNames = allInformation.map(data => {
+            return data.name;
+        });
 
-    return onlyNames.sort((srtring1, srtring2) => {
-        return (srtring1.localeCompare(srtring2, 'en', {sensitive: 'base'}));
-    });
+        return onlyNames.sort((srtring1, srtring2) => {
+            return (srtring1.localeCompare(srtring2, 'en', {sensitive: 'base'}));
+        });
+    } catch (error) {
+        return error;
+    }   
 }
 
 export default sortedCharacters;
